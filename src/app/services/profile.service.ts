dd03fd057f310865b77e87605ee23a478a24071b
Pixel-0 <tiny0pixel@gmail.com>
2019-07-15 15:54:48 +0300

    edit service

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
+
 import 'rxjs/add/operator/map';
 import { User } from '../user';
 import { Repo } from '../repo';
